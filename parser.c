994ce69dd71bbe76203fe1945fb001e6386710e2
Sami Dadon <sami@matan-il.com>
2013-02-13 19:54:19 +0200

    Updating parser Signed-off-by: Itamar Bar-Lev Shapira <itamar@gizra.com>

--- parser.c
+++ parser.c
@@ -30,14 +30,95 @@ void parser_parse() {
 	
 	char* line;
 	char* label;
-	int ic = 0, dc = 0, line_length, line_num = 0, line_num_ofset = 0;
+	char *begin_of_word,*end_of_word;
+	int ic = 0, dc = 0, line_length, line_num = 0, line_num_ofset = 0,i;
 	
 	
 	while ((line = reader_get_line())) {
 		line_num++;
+
+		if (*line==';') //this is for remark line
+			continue;
+
 		line_length=strlen(line);
 		label = parser_get_label(line);
-		printf("%s\n", label);
+
+		begin_of_word=line;
+		if (label)
+			begin_of_word=line+strlen(label)+1; //begin_of_word points to first char after label
+
+		while (*begin_of_word==' ' || *begin_of_word=='\t')
+			begin_of_word++;
+
+		if (*begin_of_word=='\0' && !label) //this is an empty line
+			continue;
+		
+		line_num_ofset++;		
+
+		end_of_word=begin_of_word+1;//find end of command
+		while (*end_of_word!=' ' && *end_of_word!='\t' && *end_of_word!='/' && *end_of_word!='\0')
+			end_of_word++;
+		
+		/* this is a data line */
+		if (!strncmp(begin_of_word,".data",end_of_word-begin_of_word-1) && *end_of_word!='/') {
+
+			printf("\nthis is a data line\n");
+
+	/*		while (*end_of_word!='\0') {
+				while (*end_of_word=' ' or *end_of_word='\t')
+					end_of_word++;
+				begin_of_word=end_of_word;
+
+				if (*begin_of_word='\0')
+					continue;
+
+				while (*end_of_word!=' ' && *end_of_word!='\t' && *end_of_word!='\0' *end_of_word!=',')
+					end_of_word++; */
+				
+				
+			continue;
+		}
+
+		/* this is a string line */
+		if (!strncmp(begin_of_word,".string",end_of_word-begin_of_word-1) && *end_of_word!='/') {
+			
+			begin_of_word=end_of_word;
+
+			while (*begin_of_word==' ' || *begin_of_word=='\t')
+				begin_of_word++;
+
+			if (*begin_of_word!='"') {
+				printf ("\n%d error, string expected after .string\n",line_num);
+				continue;
+			}
+			
+			end_of_word=begin_of_word+1;
+			while (*end_of_word!=' ' && *end_of_word!='\t' && *end_of_word!='\0')
+				end_of_word++;
+			if (*(end_of_word-1)!='"') {
+				printf ("\n%d error, string expected after .string\n",line_num);
+				continue;
+			}
+
+			printf("\nthis is a string line, the string is %s\n",begin_of_word);
+
+			while (*end_of_word==' ' || *end_of_word=='\t' && *end_of_word!='\0')
+				end_of_word++;	
+
+			if (*end_of_word!='\0')	
+				printf("%d worning, additional unused charecters at end of line",line_num);
+			continue;
+		}
+		
+		for (i=0;i<16 && strncmp(begin_of_word,instruction_list[i].instruction,end_of_word-begin_of_word-1);i++);
+		if (i==16) {
+			printf("\nerror in line %d unknown instruction\n",line_num);
+			continue;
+		}
+	
+				
+
+		printf("\n%d label is %s command is %s line is %s\n",line_num, label,instruction_list[i].instruction,line);
 		free(label);
 		free(line);
 	}
@@ -60,7 +141,8 @@ char* parser_get_label(const char* line) {
 	}
 		
 
-	while(((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z')) && len <= MAX_LABEL_SIZE + 1) {
+	while(((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z') || 
+		(*c >= '0' && *c <= '9')) && len <= MAX_LABEL_SIZE + 1) {
 		c++;
 		len++;
 	}
